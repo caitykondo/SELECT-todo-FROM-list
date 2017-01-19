e89a652eb9ef8023c076495a2e11881073df197b
Caity Kondo <caitykondo@Caitys-MacBook-Pro-2.local>
2017-01-18 22:05:57 -1000

    completed all tasks

---
+++ todo_app.sql
@@ -0,0 +1,34 @@
+DROP USER "michael";
+CREATE USER "michael";
+DROP DATABASE IF EXISTS "todo_app";
+CREATE DATABASE "todo_app";
+
+\c todo_app;
+
+CREATE TABLE "tasks" (
+  id serial PRIMARY KEY NOT NULL,
+  title varchar(255), description text,
+  created_at timestamp without time zone,
+  updated_at timestamp without time zone,
+  completed boolean
+);
+
+ALTER TABLE "tasks" DROP COLUMN completed;
+ALTER TABLE "tasks" ADD COLUMN completed_at timestamp;
+-- ALTER TABLE "tasks" ALTER COLUMN completed_at SET DEFAULT null;
+ALTER TABLE "tasks" ALTER COLUMN updated_at SET NOT NULL;
+ALTER TABLE "tasks" ALTER COLUMN updated_at SET DEFAULT now();
+INSERT INTO "tasks" VALUES (default, 'Study SQL', 'Complete this exercise', now(), now(), null);
+INSERT INTO "tasks" (title, description) VALUES('Study PostgreSQL', 'Read all the documentation');
+SELECT * FROM "tasks" WHERE completed_at = null;
+UPDATE "tasks" SET completed_at = now() WHERE title = 'Study SQL';
+SELECT tasks.title, tasks.description FROM "tasks" WHERE completed_at = null;
+SELECT * FROM "tasks" ORDER BY created_at DESC;
+INSERT INTO "tasks" (title, description) VALUES ('mistake 1', 'a test entry');
+INSERT INTO "tasks" (title, description) VALUES ('mistake 2', 'another a test entry');
+INSERT INTO "tasks" (title, description) VALUES ('third mistake', 'another a test entry');
+SELECT * FROM "tasks" WHERE title::text LIKE '%mistake%';
+DELETE FROM "tasks" WHERE title = 'mistake 1';
+SELECT tasks.title, tasks.description FROM "tasks" WHERE title::text LIKE '%mistake%';
+DELETE FROM "tasks" WHERE title::text LIKE '%mistake%';
+SELECT * FROM "tasks" ORDER BY title ASC;
