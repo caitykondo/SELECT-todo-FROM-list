6a0e2366b2ca67f8675260108d116037673a61aa
Caity Kondo <caitykondo@Caitys-MacBook-Pro-2.local>
2017-01-19 20:48:09 -1000

    addressed issue with checking for null value

--- todo_app.sql
+++ todo_app.sql
@@ -15,12 +15,11 @@ CREATE TABLE "tasks" (
 
 ALTER TABLE "tasks" DROP COLUMN completed;
 ALTER TABLE "tasks" ADD COLUMN completed_at timestamp;
--- ALTER TABLE "tasks" ALTER COLUMN completed_at SET DEFAULT null;
 ALTER TABLE "tasks" ALTER COLUMN updated_at SET NOT NULL;
 ALTER TABLE "tasks" ALTER COLUMN updated_at SET DEFAULT now();
 INSERT INTO "tasks" VALUES (default, 'Study SQL', 'Complete this exercise', now(), now(), null);
 INSERT INTO "tasks" (title, description) VALUES('Study PostgreSQL', 'Read all the documentation');
-SELECT * FROM "tasks" WHERE completed_at = null;
+SELECT * FROM "tasks" WHERE completed_at IS null;
 UPDATE "tasks" SET completed_at = now() WHERE title = 'Study SQL';
 SELECT tasks.title, tasks.description FROM "tasks" WHERE completed_at = null;
 SELECT * FROM "tasks" ORDER BY created_at DESC;
